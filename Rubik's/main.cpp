08544e670d283cf7b0812cfb8288a01b4e77ed89
Gibek-Banan <arekjozwiak21@gmail.com>
2022-09-13 22:35:38 +0200

    up

--- Rubik's/main.cpp
+++ Rubik's/main.cpp
@@ -2,7 +2,7 @@
 // PVS-Studio Static Code Analyzer for C, C++ and C#: http://www.viva64.com
 #include "Cube.hpp"
 #include "Solver.hpp"
-#include "Rubik.hpp"
+#include "rubik.hpp"
 #include <fstream>
 #include <iostream>
 #include <string>
@@ -34,12 +34,23 @@ string translate(const string& path)
 {
 	string ret;
 
-	for (size_t i = 0; path[i]; i++)
+	/*for (size_t i = 0; path[i]; i++)
 	{
 		if (path[i] == '1')
 			ret += " ";
 		else if (path[i] == '3')
-			ret += "\' ";
+			ret += "' ";
+		else if (path[i] == '2')
+			ret += "2 ";
+		else
+			ret += path[i];
+	}*/
+	for (size_t i = 0; path[i]; i++)
+	{
+		if (path[i] == '1')
+			ret += "1 ";
+		else if (path[i] == '3')
+			ret += "3 ";
 		else if (path[i] == '2')
 			ret += "2 ";
 		else
@@ -140,111 +151,220 @@ int main(int ac, char** av)
 		std::cout << "Can't open a file!" << std::endl;
 	}
 
-	//ANIMATION:
-	std::cout << "\n-------------------------------------------------\n";
-	std::cout << "CLICK ENTER TO START ANIMATION\n";
-	std::cout << "\-------------------------------------------------\n";
-	std::cin.get();
-	cube.readFromFile("colors.txt");
-	cube.convertColorsToNotation();
-	cube.getPosOri();
-	cube.show();
-	enum class Moves
+	//display.txt
+	std::ofstream file_d("display.txt");
+	if (file_d.is_open())
 	{
-		R, R2, Ri,
-		L, L2, Li,
-		U, U2, Ui,
-		D, D2, Di,
-		F, F2, Fi,
-		B, B2, Bi,
-	};
-	auto moves = split(outputTranslated, ' ');
-	std::unordered_map<std::string, Moves> m_moves =
-	{
-		{"R",Moves::R},
-		{"R2",Moves::R2},
-		{"R'",Moves::Ri},
-		{"L",Moves::L},
-		{"L2",Moves::L2},
-		{"L'",Moves::Li},
-		{"U",Moves::U},
-		{"U2",Moves::U2},
-		{"U'",Moves::Ui},
-		{"D",Moves::D},
-		{"D2",Moves::D2},
-		{"D'",Moves::Di},
-		{"B",Moves::B},
-		{"B2",Moves::B2},
-		{"B'",Moves::Bi},
-		{"F",Moves::F},
-		{"F2",Moves::F2},
-		{"F'",Moves::Fi}
-	};
+		cube.readFromFile("colors.txt");
+		cube.convertColorsToNotation();
+		cube.getPosOri();
 
-	for (const auto& m : moves)
-	{
-		switch (m_moves[m])
+		enum class Moves
 		{
-		case Moves::R:
-			cube.rotCube('R', 1);
-			break;
-		case Moves::R2:
-			cube.rotCube('R', 2);
-			break;
-		case Moves::Ri:
-			cube.rotCube('R', -1);
-			break;
-		case Moves::L:
-			cube.rotCube('L', 1);
-			break;
-		case Moves::L2:
-			cube.rotCube('L', 2);
-			break;
-		case Moves::Li:
-			cube.rotCube('L', -1);
-			break;
-		case Moves::B:
-			cube.rotCube('B', 1);
-			break;
-		case Moves::B2:
-			cube.rotCube('B', 2);
-			break;
-		case Moves::Bi:
-			cube.rotCube('B', -1);
-			break;
-		case Moves::F:
-			cube.rotCube('F', 1);
-			break;
-		case Moves::F2:
-			cube.rotCube('F', 2);
-			break;
-		case Moves::Fi:
-			cube.rotCube('F', -1);
-			break;
-		case Moves::U:
-			cube.rotCube('U', 1);
-			break;
-		case Moves::U2:
-			cube.rotCube('U', 2);
-			break;
-		case Moves::Ui:
-			cube.rotCube('U', -1);
-			break;
-		case Moves::D:
-			cube.rotCube('D', 1);
-			break;
-		case Moves::D2:
-			cube.rotCube('D', 2);
-			break;
-		case Moves::Di:
-			cube.rotCube('D', -1);
-			break;
+			R, R2, Ri,
+			L, L2, Li,
+			U, U2, Ui,
+			D, D2, Di,
+			F, F2, Fi,
+			B, B2, Bi,
+		};
+		auto moves = split(outputTranslated, ' ');
+		std::unordered_map<std::string, Moves> m_moves =
+		{
+			{"R1",Moves::R},
+			{"R2",Moves::R2},
+			{"R3",Moves::Ri},
+			{"L1",Moves::L},
+			{"L2",Moves::L2},
+			{"L3",Moves::Li},
+			{"U1",Moves::U},
+			{"U2",Moves::U2},
+			{"U3",Moves::Ui},
+			{"D1",Moves::D},
+			{"D2",Moves::D2},
+			{"D3",Moves::Di},
+			{"B1",Moves::B},
+			{"B2",Moves::B2},
+			{"B3",Moves::Bi},
+			{"F1",Moves::F},
+			{"F2",Moves::F2},
+			{"F3",Moves::Fi}
+		};
+
+		for (const auto& m : moves)
+		{
+			switch (m_moves[m])
+			{
+			case Moves::R:
+				cube.rotCube('R', 1);
+				break;
+			case Moves::R2:
+				cube.rotCube('R', 2);
+				break;
+			case Moves::Ri:
+				cube.rotCube('R', -1);
+				break;
+			case Moves::L:
+				cube.rotCube('L', 1);
+				break;
+			case Moves::L2:
+				cube.rotCube('L', 2);
+				break;
+			case Moves::Li:
+				cube.rotCube('L', -1);
+				break;
+			case Moves::B:
+				cube.rotCube('B', 1);
+				break;
+			case Moves::B2:
+				cube.rotCube('B', 2);
+				break;
+			case Moves::Bi:
+				cube.rotCube('B', -1);
+				break;
+			case Moves::F:
+				cube.rotCube('F', 1);
+				break;
+			case Moves::F2:
+				cube.rotCube('F', 2);
+				break;
+			case Moves::Fi:
+				cube.rotCube('F', -1);
+				break;
+			case Moves::U:
+				cube.rotCube('U', 1);
+				break;
+			case Moves::U2:
+				cube.rotCube('U', 2);
+				break;
+			case Moves::Ui:
+				cube.rotCube('U', -1);
+				break;
+			case Moves::D:
+				cube.rotCube('D', 1);
+				break;
+			case Moves::D2:
+				cube.rotCube('D', 2);
+				break;
+			case Moves::Di:
+				cube.rotCube('D', -1);
+				break;
+			}
+			cube.colorToWalls();
+			cube.fixWalls();
+			file_d << " " << cube.walls[U].substr(0, 3) << "    " << cube.walls[L].substr(3, 3) << "    " << cube.walls[F].substr(3, 3) << std::endl;
+			file_d << " " << cube.walls[U].substr(3, 3) << "    " << cube.walls[L].substr(0, 3) << "    " << cube.walls[F].substr(0, 3) << std::endl;
+			file_d << " " << cube.walls[U].substr(6, 3) << "    " << cube.walls[L].substr(6, 3) << "    " << cube.walls[F].substr(6, 3) << std::endl;
+			file_d << " " << cube.walls[R].substr(3, 3) << "    " << cube.walls[B].substr(3, 3) << "    " << cube.walls[D].substr(0, 3) << std::endl;
+			file_d << " " << cube.walls[R].substr(0, 3) << "    " << cube.walls[B].substr(0, 3) << "    " << cube.walls[D].substr(3, 3) << std::endl;
+			file_d << " " << cube.walls[R].substr(6, 3) << "    " << cube.walls[B].substr(6, 3) << "    " << cube.walls[D].substr(6, 3) << std::endl;
 		}
-		system("CLS");
-		cube.show();
-		std::this_thread::sleep_for(100ms);
 	}
 
+	////ANIMATION:
+	//std::cout << "\n-------------------------------------------------\n";
+	//std::cout << "CLICK ENTER TO START ANIMATION\n";
+	//std::cout << "\-------------------------------------------------\n";
+	//std::cin.get();
+	//cube.readFromFile("colors.txt");
+	//cube.convertColorsToNotation();
+	//cube.getPosOri();
+	//cube.show();
+	//enum class Moves
+	//{
+	//	R, R2, Ri,
+	//	L, L2, Li,
+	//	U, U2, Ui,
+	//	D, D2, Di,
+	//	F, F2, Fi,
+	//	B, B2, Bi,
+	//};
+	//auto moves = split(outputTranslated, ' ');
+	//std::unordered_map<std::string, Moves> m_moves =
+	//{
+	//	{"R",Moves::R},
+	//	{"R2",Moves::R2},
+	//	{"R'",Moves::Ri},
+	//	{"L",Moves::L},
+	//	{"L2",Moves::L2},
+	//	{"L'",Moves::Li},
+	//	{"U",Moves::U},
+	//	{"U2",Moves::U2},
+	//	{"U'",Moves::Ui},
+	//	{"D",Moves::D},
+	//	{"D2",Moves::D2},
+	//	{"D'",Moves::Di},
+	//	{"B",Moves::B},
+	//	{"B2",Moves::B2},
+	//	{"B'",Moves::Bi},
+	//	{"F",Moves::F},
+	//	{"F2",Moves::F2},
+	//	{"F'",Moves::Fi}
+	//};
+
+	//for (const auto& m : moves)
+	//{
+	//	switch (m_moves[m])
+	//	{
+	//	case Moves::R:
+	//		cube.rotCube('R', 1);
+	//		break;
+	//	case Moves::R2:
+	//		cube.rotCube('R', 2);
+	//		break;
+	//	case Moves::Ri:
+	//		cube.rotCube('R', -1);
+	//		break;
+	//	case Moves::L:
+	//		cube.rotCube('L', 1);
+	//		break;
+	//	case Moves::L2:
+	//		cube.rotCube('L', 2);
+	//		break;
+	//	case Moves::Li:
+	//		cube.rotCube('L', -1);
+	//		break;
+	//	case Moves::B:
+	//		cube.rotCube('B', 1);
+	//		break;
+	//	case Moves::B2:
+	//		cube.rotCube('B', 2);
+	//		break;
+	//	case Moves::Bi:
+	//		cube.rotCube('B', -1);
+	//		break;
+	//	case Moves::F:
+	//		cube.rotCube('F', 1);
+	//		break;
+	//	case Moves::F2:
+	//		cube.rotCube('F', 2);
+	//		break;
+	//	case Moves::Fi:
+	//		cube.rotCube('F', -1);
+	//		break;
+	//	case Moves::U:
+	//		cube.rotCube('U', 1);
+	//		break;
+	//	case Moves::U2:
+	//		cube.rotCube('U', 2);
+	//		break;
+	//	case Moves::Ui:
+	//		cube.rotCube('U', -1);
+	//		break;
+	//	case Moves::D:
+	//		cube.rotCube('D', 1);
+	//		break;
+	//	case Moves::D2:
+	//		cube.rotCube('D', 2);
+	//		break;
+	//	case Moves::Di:
+	//		cube.rotCube('D', -1);
+	//		break;
+	//	}
+	//	system("CLS");
+	//	cube.show();
+	//	std::this_thread::sleep_for(100ms);
+	//}
 
 	//NUMERATION
 	/* cube.show();
